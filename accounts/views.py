e3aeda29c5432fb7fb57805a5ff34b0305c60f5e
nanthu0123 <nanthu0123@gmail.com>
2020-03-02 21:20:33 +0530

    signup page
    signup page created

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def signup(request):
+
+    if request.method=='POST':
+        if request.POST['password1']==request.POST['password2']:
+            try:
+                user=User.objects.get(username=request.POST['username'])
+                return render(request,'accounts/signup.html',{'error':'username is already exist'})
+            
+            except User.DoesNotExist:
+                user=User.objects.create_user(request.POST['username'],password=request.POST['password1'])
+                auth.login(request,user)
+                return redirect('home')
+
+
+
     return render(request, 'accounts/signup.html')
 
 def login(request):
