dd0bae8ec87290efb5053198e8497c238666c961
nanthu0123 <nanthu0123@gmail.com>
2020-03-03 22:19:14 +0530

    static file added,detail file created
    PA

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 from django.utils import timezone
@@ -23,9 +23,13 @@ def create(request):
             product.image=request.FILES['image']
             product.pub_date=timezone.datetime.now()
             product.hunter=request.user
-            product.save()
-            return redirect('home')
+            product.save() 
+            return redirect('/products/'+str(product.id))
         else:
             return render(request,'products/create.html',{'error':'all fields are required to fill'})
     else:        
         return render(request,'products/create.html')
+
+def detail(request,product_id):
+    product=get_object_or_404(Product, pk=product_id)
+    return render(request,'products/detail.html',{'product':product})
