521abb0f2fc5342b3707841fe4a5c7e5f3833c02
nanthu0123 <nanthu0123@gmail.com>
2020-03-03 23:29:08 -0800

    home page,upvote,detail page
    upvote icon and logic developed,detail page created,home page added, new users redirected to signup page

--- products/views.py
+++ products/views.py
@@ -4,9 +4,10 @@ from .models import Product
 from django.utils import timezone
 
 def home(request):
-    return render(request,'products/home.html')
+    products=Product.objects
+    return render(request,'products/home.html',{'products':products})
 
-@login_required
+@login_required(login_url="/accounts/signup")
 
 def create(request):
     if request.method=='POST':
@@ -33,3 +34,11 @@ def create(request):
 def detail(request,product_id):
     product=get_object_or_404(Product, pk=product_id)
     return render(request,'products/detail.html',{'product':product})
+
+@login_required (login_url="/accounts/signup")
+def upvote(request,product_id):
+    if request.method=='POST':
+        product=get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/products/'+str(product.id))
